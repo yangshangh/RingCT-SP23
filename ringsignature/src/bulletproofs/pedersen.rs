1a06ef7d23a47a4c68ceba50dbc35d7e6866e41d
Franklin <egoistzty@gmail.com>
2024-08-30 20:45:41 +0800

    complete the pedersen commitment

--- ringsignature/src/bulletproofs/pedersen.rs
+++ ringsignature/src/bulletproofs/pedersen.rs
@@ -1,8 +1,7 @@
 use ark_ec::CurveGroup;
-use ark_std::{rand::Rng, UniformRand};
+use ark_std::{rand::Rng, UniformRand, Zero};
 
-
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Params<C: CurveGroup> {
     pub g: C,
     pub h: C,
@@ -10,32 +9,55 @@ pub struct Params<C: CurveGroup> {
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Pedersen<C: CurveGroup> {
+    pub params: Params<C>,
     pub commitment : C,
     message: C::ScalarField,
-    randnom: C::ScalarField,
+    random: C::ScalarField,
 }
 
 impl<C: CurveGroup> Pedersen<C> {
-    pub fn setup<R: Rng>(rng: &mut R) -> Params<C> {
-        // let h_r = C::ScalarField::rand(rng);
-        let g = C::rand(rng);
-        let h = C::rand(rng);
-        Params {
-            g,
-            h,
+    pub fn new() -> Self {
+        Self {
+            params: Params {
+                g: C::generator(),
+                h: C::generator(),
+            },
+            commitment: C::generator(),
+            message: C::ScalarField::zero(),
+            random: C::ScalarField::zero(),
         }
     }
 
-    pub fn commit(
-        params: &Params<C>,
-        message: &C::ScalarField,
-        random: &C::ScalarField,
-    ) -> C {
-        let gm = params.g.mul(message);
-        let hr = params.h.mul(random);
-
-        gm + hr
+    pub fn setup<R: Rng>(&mut self, rng: &mut R) {
+        let g = C::rand(rng);
+        let h = C::rand(rng);
+        self.params.g = g;
+        self.params.h = h;
     }
+
+    pub fn commit<R: Rng>(
+        &mut self,
+        message: &C::ScalarField,
+        rng: &mut R,
+    ) {
+        self.message = message.clone();
+        self.random = C::ScalarField::rand(rng);
+        let gm = self.params.g.mul(self.message);
+        let hr = self.params.h.mul(self.random);
+
+        self.commitment = gm + hr;
+    }
+
+    pub fn open(&self) -> (&C::ScalarField, &C::ScalarField) {
+        (&self.message, &self.random)
+    }
+}
+
+fn verify<C: CurveGroup>(pedcom: &Pedersen::<C>, message: &C::ScalarField, random: &C::ScalarField) {
+    let gm = pedcom.params.g.mul(message);
+    let hr = pedcom.params.h.mul(random);
+
+    assert_eq!(pedcom.commitment, gm + hr);
 }
 
 #[cfg(test)]
@@ -46,12 +68,14 @@ mod tests {
     #[test]
     fn tets_pedersen() {
         let mut rng = ark_std::test_rng();
-
-        let params = Pedersen::setup(&mut rng);
+        let mut pedcom = Pedersen::<Projective>::new();
         
-        let m: Fr= Fr::rand(&mut rng);
-        let r: Fr= Fr::rand(&mut rng);
-        let cm = Pedersen::<Projective>::commit(&params, &m, &r);
-        println!("cm: {:?}", cm);
+        pedcom.setup(&mut rng);
+        
+        let m: Fr= Fr::from(114514);
+        pedcom.commit(&m, &mut rng);
+        
+        let (mprime, rprime) = pedcom.open();
+        verify(&pedcom, &mprime, &rprime);
     }
  }
